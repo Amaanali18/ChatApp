26f29c223967e35183abe06fc0d2859111db55df
Amaanali Parchigar <amaanaliparchigar0918@gmail.com>
2026-06-21 11:51:27 +0530

    Audited some last changes

--- backend/src/main/java/com/amaan/backend/BackendApplication.java
+++ backend/src/main/java/com/amaan/backend/BackendApplication.java
@@ -1,13 +1,13 @@
 package com.amaan.backend;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
--- frontend/src/config/AxiosHelper.js
+++ frontend/src/config/AxiosHelper.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 export const baseurl = import.meta.env.VITE_BACKEND_URL
 export const AxiosApi = axios.create({
-    baseURL: baseurl
+    baseURL: baseurl,
+    headers: {
+        "X-CLIENT-KEY": import.meta.env.VITE_CLIENT_KEY
+    }
 });
