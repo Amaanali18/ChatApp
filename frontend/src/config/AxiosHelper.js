04e2f0c4db59ae7070e1f19a46ea4e1c81c5355d
Amaanali Parchigar <amaanaliparchigar0918@gmail.com>
2026-06-21 14:42:27 +0530

    Audited some last changes

--- frontend/src/config/AxiosHelper.js
+++ frontend/src/config/AxiosHelper.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 export const baseurl = import.meta.env.VITE_BACKEND_URL
 export const AxiosApi = axios.create({
-    baseURL: baseurl,
-    headers: {
-        "X-CLIENT-KEY": import.meta.env.VITE_CLIENT_KEY
-    }
+    baseURL: baseurl
 });
